2ec892d96039bedf781e6bfe4b16f2dec5d104d0
Mackenzie McIntyre <362574782@GoTVDSB.ca>
2026-03-11 13:20:15 -0400

    almost done make it look pretty

--- Program.cs
+++ Program.cs
@@ -63,38 +63,119 @@
             string yesOrNoBet;
             double bettingAmount;
             bool done=false;
-            int round = 1;
-            int bettingMax = 200;
+            int userChoice;
+            double newBalance;
+            string playAgain;
+            
             int rock = 1, paper = 2, scissors = 3;
 
 
-            //Random generator = new Random();
-            //int randNum; 
-            //randNum = generator.Next(1,4);
+            Random generator = new Random();
+            int randNum;
+            randNum = generator.Next(1, 4);
 
             while (!done)
             {
                 Console.Clear();
                 Console.WriteLine("\t \t \t \t \t \t WELCOME TO THE GAME!");
-                Console.WriteLine("Right now you have $" + bankAccount + ". How much would you like to bet? your betting max is $200 (Just type the number)");
-                
-                while(!double.TryParse(Console.ReadLine(), out bettingAmount) || bettingAmount < 0 || bettingAmount > bettingMax)
+                Console.WriteLine("Right now you have $" + bankAccount + ". How much would you like to bet?");
+
+                while (!double.TryParse(Console.ReadLine(), out bettingAmount) || bettingAmount < 0 || bettingAmount > bankAccount)
                 {
-                    Console.WriteLine("Invalid bet. Try again");
+                    Console.WriteLine("Invalid bet. Enter a bet:");
+                }    
+                    
+                Console.Clear() ;  
+                Console.WriteLine("Okay! You are betting $" + bettingAmount + " on this round!");
+                Console.WriteLine("Choose one of the following:");
+                Console.WriteLine(rock + ".) Rock");
+                Console.WriteLine(paper + ".) Paper");
+                Console.WriteLine(scissors+ ".) Scissors");
+                Console.WriteLine("Please just pick the number!");
+                //int.TryParse(Console.ReadLine(), out userChoice);
+
+                while (!int.TryParse(Console.ReadLine(), out userChoice) || userChoice < 0 || userChoice > 3) 
+                {
+                    Console.WriteLine("Im sorry that is not an option! Enter your choice:");
+                    
+                }
+
+                Console.Clear();
+                if (userChoice == rock && randNum == paper)
+                {
+                    newBalance = bankAccount - bettingAmount;
+                    Console.WriteLine("Sorry! the computer chose paper! you lose $" + bettingAmount);
+                    Console.WriteLine("You now have $" + newBalance);
                     Console.ReadLine();
+                    
 
                 }
-                
-                    Console.Clear() ;
-                    Console.WriteLine("Okay! You are betting " + bettingAmount + " on this round!");
-                    Console.WriteLine("Choose one of the following:");
-                    Console.WriteLine("\t Rock" + rock);
-                    Console.WriteLine("\t 2.) paper");
-                    Console.WriteLine("\t 3.) scissors");
+                else if (userChoice == rock && randNum == scissors)
+                {
+                    newBalance = bankAccount + bettingAmount;
+                    Console.WriteLine("Congrats the computer picked scissors! You win " + bettingAmount);
+                    Console.WriteLine("You now have $" + newBalance);
+                    Console.ReadLine();
+                 
+                   
 
-                
-               
+                }
+                else if (userChoice == paper && randNum == scissors)
+                {
+                    newBalance = bankAccount - bettingAmount;
+                    Console.WriteLine("Sorry! the computer picked scissors! you lose $" + bettingAmount);
+                    Console.WriteLine("You now have $" + newBalance);
+                    Console.ReadLine();
+                   
 
+                }
+                else if (userChoice == paper && randNum == rock)
+                {
+                    newBalance = bankAccount + bettingAmount;
+                    Console.WriteLine("Congrats! the computer picked rock! you win $" + bettingAmount);
+                    Console.WriteLine("You now have $" + newBalance);
+                    Console.ReadLine();
+                    
+
+                }
+                else if (userChoice == scissors && randNum == rock)
+                {
+                    newBalance = bankAccount - bettingAmount;
+                    Console.WriteLine("Sorry! the computer picked rock! you lose $" + bettingAmount);
+                    Console.WriteLine("You now have $" + newBalance);
+                    Console.ReadLine();
+                    
+
+                }
+                else if (userChoice == scissors && randNum == paper)
+                {
+                    newBalance = bankAccount + bettingAmount;
+                    Console.WriteLine("Congrats! the computer picked paper! you win $" + bettingAmount);
+                    Console.WriteLine("You now have $" + newBalance);
+                    Console.ReadLine();
+                    
+                }
+                else if (userChoice == paper && randNum == paper || userChoice == scissors && randNum == scissors || userChoice == rock && randNum == rock)
+                {
+                    Console.WriteLine("The computer also picked that! You tie! pres ENTER to try again!");
+                    Console.ReadLine();
+                }
+
+                Console.WriteLine("Thanks for playing! Would you like to play again?");
+                Console.WriteLine("YES or NO");
+
+                playAgain=Console.ReadLine();
+                if(playAgain.ToLower() == "yes")
+                {
+                    Console.WriteLine("Goodluck!");
+                    //bankaccount= newBAlanace
+                    Part2();
+                }
+                else if(playAgain.ToLower() == "no")
+                {
+                    Console.WriteLine("Have a great rest of your day! Thanks for playing!");
+                    done=true;
+                }
 
 
 
